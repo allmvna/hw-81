c2b2066a82f7d2d134dd6cb69a3407fb33198029
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-12-26 15:56:49 +0600

    An express server is configured with a MongoDB connection and CORS support

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,27 @@
 import express from 'express';
 import cors from 'cors';
+import linksRouter from "./routes/links/links";
+import mongoDb from "./mongoDb";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/links', linksRouter);
+
 
 const run = async () => {
+    await mongoose.connect('mongodb://localhost/app');
 
     app.listen(port, () => {
         console.log(`Server started on http://localhost:${port}`);
     });
+
+    process.on('exit', err => {
+        mongoDb.disconnect();
+    })
 };
 
 run().catch(e => console.error(e));
